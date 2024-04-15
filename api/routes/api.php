7c1343fe027802bf5e38270dfa15e2ec104327ed
DaviSouzaES <dribeiro@olie.ai>
2024-04-15 12:03:06 -0300

    🪧route: first routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,30 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// FEITO
+Route::group(["prefix" => "/auth"], function() {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+});
+
+Route::group(["prefix" => "/user"], function() {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/', [UserController::class, 'show'])->name('show');
+        Route::get('/confirm/{id}', [UserController::class, 'confirmParticipation'])->name('confirmParticipation');
+    });
+});
+
+Route::group(["prefix" => "/event"], function() {
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::get('/{id}', [UserController::class, 'show'])->name('show');
+    Route::post('/', [UserController::class, 'create'])->name('create');
+    Route::put('/{id}', [UserController::class, 'update'])->name('update');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
+    Route::post('/invite/{id}', [UserController::class, 'destroy'])->name('destroy');
 });
