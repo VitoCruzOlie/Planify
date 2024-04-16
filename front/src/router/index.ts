e0046fc9a6199418776d6750d18d58a85946e6d3
VitoCruzOlie <vsouza@olie.ai>
2024-04-16 16:10:27 -0300

    feat- create login screen

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Register from '../views/Register.vue';
+import Login from '../views/Login.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +15,12 @@ const router = createRouter({
         path: '/register',
         name: 'register',
         component: Register
-      }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login
+      },
     ]
   })
   
