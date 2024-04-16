de49b919b1bb00a84813e2b058283ee1886f3703
VitoCruzOlie <vsouza@olie.ai>
2024-04-16 16:15:01 -0300

    feat- LoginForm validation

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 //Component imports
-
+import LoginForm from '@/components/Forms/LoginForm.vue';
 
 </script>
 <template>
@@ -8,22 +8,18 @@
     class="w-full flex flex-row justify-between p-2 items-center border border-b-neutral-300 shadow-md shadow-black"
   >
     <h1 class="text-primary text-xl font-bold">Planify</h1>
-    <button class="font-bold text-xs">
-      <p>Entrar</p>
-    </button>
   </nav>
   <main class="w-full min-h-svh bg-white">
     <div class="w-full flex flex-col gap-7">
       <div class="flex flex-col gap-2 justify-center items-center text-center pt-16">
         <p class="font-bold text-base">
-          Crie uma conta para <br />
-          iniciar sua experiência com Planify!
+          Entre ma sua conta!
         </p>
         <p class="font-light text-sm">
-          Faltam só mais alguns passos!<br />Nós também detestamos formulários.
+          só mais alguns cliques!<br />Nós também detestamos formulários.
         </p>
       </div>
-      <RegisterForm/>
+      <LoginForm/>
     </div>
   </main>
 </template>
