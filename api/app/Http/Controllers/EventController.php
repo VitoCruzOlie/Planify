b1d08cbf2338c6c7d70ef9d0c1b0ed45d8cad285
DaviSouzaES <dribeiro@olie.ai>
2024-04-16 09:29:02 -0300

    ⚙️feat: event controller

--- api/app/Http/Controllers/EventController.php
+++ api/app/Http/Controllers/EventController.php
@@ -2,64 +2,166 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EventRequest;
 use App\Models\Event;
+use App\Models\EventUser;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(protected Event $repository)
+    {
+
+    }
     public function index()
     {
-        //
+        try {
+            $events = $this->repository->with('users')->get();
+            return response()->json([
+                "message" => "Sucess",
+                "data" => $events
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(EventRequest $request)
     {
-        //
+        try {
+            $event = $request->validated();
+            $user = $request->user();
+
+            $eventCreated = $this->repository->create($event);
+            $eventCreated->users()->attach($user);
+
+            $invite = EventUser::where('event_id', $eventCreated->id)
+                ->where('user_id', $user->id)
+                ->first();
+
+            $invite->update(['confirmed' => true]);
+
+            return response()->json([
+                "message" => "Event create with sucess",
+                "data" => $eventCreated
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show(Request $request)
     {
-        //
+        try {
+            $event = $this->repository->with('users')->findOrFail($request->id);
+
+            return response()->json([
+                "message" => "Sucess",
+                "data" => $event
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Event $event)
+    public function update(Request $request)
     {
-        //
+        try {
+            $event = $this->repository->findOrFail($request->id);
+            $event->update($request->all());
+            return response()->json([
+                "message" => "Sucess",
+                "data" => $event
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Event $event)
+    public function destroy(Request $request)
     {
-        //
+        try {
+            $this->repository->findOrFail($request->id)
+                ->delete();
+
+            return response()->json([
+                "message" => "",
+                "data" => []
+            ], Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function invite(Request $request)
+    {
+        try {
+            $eventId = $request->id;
+            $user = User::where("email", $request->email)->firstOrFail();
+
+            $invite = EventUser::where('event_id', $eventId)
+                ->where('user_id', $user->id)
+                ->first();
+
+            if ($invite) {
+                return response()->json([
+                    "message" => "This person is areadly invited",
+                    "data" => []
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            $event = $this->repository->findOrFail($eventId);
+
+            $event->users()->attach($user);
+
+            return response()->json([
+                "message" => "Invite submit with sucess",
+                "data" => new EventResource($event->with('users')->find($eventId))
+            ], Response::HTTP_CREATED);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Event $event)
+    public function removeInvite(Request $request)
     {
-        //
-    }
+        try {
+            $user = User::where("email", $request->email)->firstOrFail();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Event $event)
-    {
-        //
+            EventUser::where('event_id', $request->id)
+                ->where('user_id', $user->id)
+                ->delete();
+
+            return response()->json([
+                "message" => "",
+                "data" => []
+            ], Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
