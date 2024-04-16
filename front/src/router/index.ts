20613e29d1bb7613ce11d0103afad680e89d9042
DaviSouzaES <dribeiro@olie.ai>
2024-04-16 14:56:21 -0300

    🪧router: calendar route

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import CalendarView from '../views/CalendarView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
         name: 'home',
         component: Home
       },
+      {
+        path: '/calendar',
+        name: 'calendar',
+        component: CalendarView
+      }
     ]
   })
   
