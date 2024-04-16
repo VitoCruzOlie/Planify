eb5b44d85645797c93d44313e531538afd6063d0
VitoCruzOlie <vsouza@olie.ai>
2024-04-16 10:36:40 -0300

    feat- Register screen

---
+++ front/src/views/Register.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <nav class="w-full flex flex-row justify-between p-2 items-center border border-b-neutral-300 shadow-md shadow-black">
+    <h1 class="text-primary text-xl font-bold">Planify</h1>
+    <button class="font-bold text-xs">
+      <p>Entrar</p>
+    </button>
+  </nav>
+  <main class="w-full min-h-svh bg-white">
+    <div class="w-full">
+        <p class="font-bold text-base  ">Crie uma conta para <br> iniciar sua experiência com Planify!</p>
+        <div>
+            <p class="font-light text-sm ">
+                Faltam só mais alguns passos!<br>Nós também detestamos formulários.
+            </p>
+        </div>
+    </div>
+
+  </main>
+</template>
