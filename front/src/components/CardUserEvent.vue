af82a6b7e427b44462364e28689ce3dee3e521e2
DaviSouzaES <dribeiro@olie.ai>
2024-04-17 14:50:14 -0300

    feat: cardUserEvent component

---
+++ front/src/components/CardUserEvent.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div class="flex items-center w-4/4 h-16 bg-slate-300 rounded-2xl mt-4" >
+        <div class="w-1/5 h-16 bg-primary rounded-2xl"></div>
+        <div>
+            <h1 class="text-black font-bold text-lg">testeeeeee</h1>
+        </div>  
+    </div>
+</template>
