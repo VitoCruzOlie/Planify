e71d9b5224ecb66eb40b171654949105a428fc29
VitoCruzOlie <vsouza@olie.ai>
2024-04-17 14:45:53 -0300

    feat- CreatEvent form

--- front/src/components/Input.vue
+++ front/src/components/Input.vue
@@ -7,7 +7,7 @@ type Props = {
 </script>
 <template>
   <input
-    class="flex flex-row gap-2 p-2  placeholder:text-neutral-500 text-sm border border-neutral-300 rounded-sm"
+    class="flex flex-row gap-2 p-2  placeholder:text-neutral-500 text-sm border border-neutral-300 rounded-sm "
     type="text"
     :placeholder="props.placeholder"
   />
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import CalendarView from '../views/CalendarView.vue';
 import Register from '../views/Register.vue';
-import Login from '../views/Login.vue'
+import Login from '../views/Login.vue';
+import CreateEvent from '@/views/CreateEvent.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
         name: 'login',
         component: Login
       },
+      {
+        path:'/create-event',
+        name: 'create-event',
+        component: CreateEvent
+      }
     ]
   })
   
