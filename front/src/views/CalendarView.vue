7f6763ffa1b0dfc67848645a90064f36d0d3a3b6
DaviSouzaES <dribeiro@olie.ai>
2024-04-17 14:50:00 -0300

    feat : calendar view

--- front/src/views/CalendarView.vue
+++ front/src/views/CalendarView.vue
@@ -1,52 +1,43 @@
 <script setup lang="ts">
+import CardUserEvent from "../components/CardUserEvent.vue";
+
 import { ref } from 'vue';
+import { useStore } from 'vuex'
 
-const MOCK_EVENT = [
-    {
-        "name": "Jogar bola",
-        "date": "2024-04-16"
-    },
-    {
-        "name": "PASSEAR NO CAMPO",
-        "date": "2024-04-20"
-    },
-    {
-        "name": "LAVAR O CACHORRO",
-        "date": "2024-04-20"
-    },
-]
-
+const store = useStore();
 
 const attrs = ref([
     {
         highlight: true,
         dates: [new Date()]
-    },
-    {
-
     }
 ]);
 
-MOCK_EVENT.forEach((e) => {
-    attrs.value.push({
-        dot: true,
-        popover: {
-            label: e.name,
-            visibility: 'hover',
-            hideIndicator: true,
-        },
-        content: "blue",
-        dates: [new Date(e.date)]
+async function load() {
+    await store.dispatch('event/getEvents')
+    store.getters['event/userEvents'].forEach(e => {
+        attrs.value.push({
+            dot: true,
+            popover: {
+                label: e.name,
+                visibility: 'hover',
+                hideIndicator: true,
+            },
+            content: "blue",
+            dates: [new Date(e.date)]
+        })
     })
-})
+}
 
-const teste = (event: any) => {
-    console.log(event);
-};
+load()
+
 </script>
 
 <template>
-    <main class="p-2 flex justify-center">
-        <VCalendar @dayclick="teste" expanded color="blue" :attributes="attrs" />
+    <main class="p-2 flex justify-center flex-col">
+        <VCalendar expanded color="blue" :attributes="attrs" />
+        <section>
+            <CardUserEvent />
+        </section>
     </main>
 </template>
