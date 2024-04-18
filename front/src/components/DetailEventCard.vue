19cbb7de32078f4d40700c94213f81174d1a2a7c
VitoCruzOlie <vsouza@olie.ai>
2024-04-18 14:22:08 -0300

    feat- add DetailEventCard

---
+++ front/src/components/DetailEventCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { PhTimer } from "@phosphor-icons/vue";
+
+const props = defineProps<{
+  title: string;
+  date: string;
+  hour: string;
+}>();
+</script>
+<template>
+  <div
+    class="flex flex-col items-center gap-2.5 hover:bg-neutral-100 transition-all group rounded-lg"
+  >
+    <div class="gap-0.5 p-2">
+      <div
+        class="rounded-lg overflow-hidden items-center justify-center gap-2.5 w-80 h-52 flex"
+      >
+        <img
+          class="group-hover:scale-105 transition-all object-cover w-full h-full"
+          src="/cardimage.svg"
+        />
+      </div>
+      <div class="w-full line-clamp-1 flex flex-col pt-2">
+        <div class="flex flex-row w-full gap-3 items-center">
+          <span class="text-black font-bold text-2xl"
+            >{{ props.title }} -
+          </span>
+          <span class="text-primary font-bold text-2xl">{{date}}</span>
+        </div>
+        <div class="flex flex-row gap-0.5 items-center">
+          <PhTimer class="text-gray-500 text-lg"/>
+          <span class="text-gray-500 font-light text-sm line-clamp-1"
+            >Quinta às {{ hour }}</span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ front/src/views/EventDetails.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DetailEventCard from '@/components/DetailEventCard.vue'
+</script>
+<template>
+  <nav>
+    <div class="flex flex-row p-4 border-b-neutral-300 border">
+      <h1 class="text-primary text-xl font-bold">Planify</h1>
+    </div>
+  </nav>
+  <main class="w-full min-h-svh bg-white">
+    <div class="gap-2.5 px-4 flex flex-col justify-center pb-4">
+      <DetailEventCard title="Reunião Top" date="18/04/2024" hour="21:20"/>
+      <div class="pl-3 flex flex-col gap-4 ">
+      <div>
+        <span class="text-sm font-semibold">ASSUNTO</span>
+        <p class="text-neutral-500 text-xs">Reunião organizacional</p>
+      </div>
+      <div>
+        <span class="text-sm font-semibold">DESCRIÇÃO DO EVENTO</span>
+        <p class="text-neutral-500 text-xs line-clamp-4">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja.</p>
+      </div>
+    </div>
+    </div>    
+  </main>
+</template>
