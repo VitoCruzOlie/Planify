ef36dc29eb719f5979bacbccc04d9f72a53e39ea
DaviSouzaES <dribeiro@olie.ai>
2024-04-17 22:15:35 -0300

    feat: add toast and register integration

--- front/src/components/Forms/RegisterForm.vue
+++ front/src/components/Forms/RegisterForm.vue
@@ -9,12 +9,19 @@ import {
 } from "@vue-hooks-form/core";
 import { z } from "zod";
 import { useZodResolver } from "@vue-hooks-form/zod";
+import { useStore } from 'vuex'
 
+const emit = defineEmits(['alertError', 'alertSucess'])
+
+// STORE
+const store = useStore()
+
+// ZOD
 const schema = z.object({
   name: z.string().min(3, {
     message: "O nome deve possúir mais que três caracteres!",
   }),
-  email: z.string().min(1,{
+  email: z.string().min(1, {
     message: "Por favor, preencha o campo de email!",
   }).email({
     message: "O email informado não é válido",
@@ -23,7 +30,7 @@ const schema = z.object({
   password: z.string().min(8, {
     message: "A senha não pode possuir menos que 8 caracteres!",
   }),
-   confirmPassword: z.string().min(8, {
+  confirmPassword: z.string().min(8, {
     message: "A senha não pode possuir menos que 8 caracteres!",
   })
 }).refine(data => data.password === data.confirmPassword, {
@@ -37,16 +44,23 @@ const form = useForm<Schema>({
   resolver: useZodResolver(schema),
 });
 
-const onSubmit = createSubmitHandler(() => {});
-const onError = createErrorHandler((errors) => {
-  console.log(errors);
+// FORM HANDLER
+
+const onSubmit = createSubmitHandler(async () => {
+  try {
+    await store.dispatch('user/register', form.getValues())
+    emit('alertSucess')
+  } catch (error) {
+    emit('alertError')
+  }
 });
+const onError = createErrorHandler((errors) => {
+  emit('alertError')
+});
+
 </script>
 <template>
-  <form
-    @submit.prevent="form.handleSubmit(onSubmit, onError)()"
-    class="gap-8 flex flex-col px-10"
-  >
+  <form @submit.prevent="form.handleSubmit(onSubmit, onError)()" class="gap-8 flex flex-col px-10">
     <div class="gap-3 flex flex-col">
       <Input :="form.register('name')" placeholder="Nome" />
       <p class="text-red-600 font-medium text-sm">
