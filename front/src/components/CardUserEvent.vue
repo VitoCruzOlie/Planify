25c500d3eb4f2c890a0c7dfd287a7513566def21
DaviSouzaES <dribeiro@olie.ai>
2024-04-17 22:15:05 -0300

    feat: card event user component

--- front/src/components/CardUserEvent.vue
+++ front/src/components/CardUserEvent.vue
@@ -1,10 +1,11 @@
 <script setup></script>
 
 <template>
-    <div class="flex items-center w-4/4 h-16 bg-slate-300 rounded-2xl mt-4" >
+    <div class="flex items-center w-4/4 h-16 bg-slate-300 rounded-2xl mt-4">
         <div class="w-1/5 h-16 bg-primary rounded-2xl"></div>
-        <div>
-            <h1 class="text-black font-bold text-lg">testeeeeee</h1>
-        </div>  
+        <div class="ml-4">
+            <slot name="tittle"></slot>
+            <slot name="description"></slot>
+        </div>
     </div>
 </template>
