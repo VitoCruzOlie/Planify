a262eaa439362e7b99ce08fb3a43d4140f80a425
VitoCruzOlie <vsouza@olie.ai>
2024-04-18 15:23:21 -0300

    feat- ParticipantSection

---
+++ front/src/components/ParticipantSection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+ }>();
+</script>
+
+
+<template>
+    <div class="p-10 bg-neutral-500 shadow-md shadow-black">
+        <p class="text-lg ">{{ props.title }}</p>
+        <div class="w-full flex flex-col gap-2">
+
+        </div>
+    </div>
+</template>
