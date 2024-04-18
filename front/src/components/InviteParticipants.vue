fc4bdd9d1065657866c516c8fbe1573a4165c878
VitoCruzOlie <vsouza@olie.ai>
2024-04-18 15:00:16 -0300

    feat- Combobox component

--- front/src/components/InviteParticipants.vue
+++ front/src/components/InviteParticipants.vue
@@ -1,9 +1,11 @@
-<script>
+<script setup lang="ts">
 import Button from '@/components/Button.vue';
+import Combobox from '@/components/Combobox.vue';
 </script>
 <template>
     <div class="w-full flex flex-col justify-center items-center">
         <span class="font-semibold text-2xl">Convide participantes!</span>
         <span class="text-neutral-500 text-sm">Convide participantes para seu evento.</span>
+        <Combobox/>
     </div>
 </template>
