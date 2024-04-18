80bc6bbc0e6d387b0b57e3959c6394d7506b4bd1
DaviSouzaES <dribeiro@olie.ai>
2024-04-18 10:31:47 -0300

    feat: error handler

--- front/src/views/CalendarView.vue
+++ front/src/views/CalendarView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import CardUserEvent from "../components/CardUserEvent.vue";
+import NavBarBottom from "../components/NavBarBottom.vue";
+
 import Skeleton from 'primevue/skeleton';
 
 import { ref } from 'vue';
 import { useStore } from 'vuex'
+
+const emit = defineEmits(['alertError'])
 
 const store = useStore();
 
@@ -19,23 +23,32 @@ let events = ref([])
 let showSkeleton = ref(false);
 
 async function load() {
-    showSkeleton.value = true;
-    await store.dispatch('event/getEvents')
-    store.getters['event/userEvents'].forEach(e => {
-        attrs.value.push({
-            dot: true,
-            popover: {
-                label: e.name,
-                visibility: 'hover',
-                hideIndicator: true,
-            },
-            content: "blue",
-            dates: [new Date(e.date)]
-        })
+    try {
+        showSkeleton.value = true;
+        await store.dispatch('event/getEvents')
 
-        events.value.push(e);
+        if (store.getters['event/userEvents']) {
+            store.getters['event/userEvents'].forEach(e => {
+                attrs.value.push({
+                    dot: true,
+                    popover: {
+                        label: e.name,
+                        visibility: 'hover',
+                        hideIndicator: true,
+                    },
+                    content: "blue",
+                    dates: [new Date(e.date)]
+                })
+
+                events.value.push(e);
+                showSkeleton.value = false
+                return
+            })
+        }
         showSkeleton.value = false
-    })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 load()
@@ -43,15 +56,19 @@ load()
 </script>
 
 <template>
-    <main class="p-2 flex justify-center flex-col">
+    <nav
+        class="w-full flex flex-row justify-between p-4 items-center">
+        <h1 class="text-primary text-xl font-bold">Planify</h1>
+    </nav>
+    <main class="p-4 flex justify-center flex-col mb-20">
         <VCalendar expanded color="blue" :attributes="attrs" />
         <section>
             <CardUserEvent v-for="(event, index) in events" :key="index">
                 <template v-slot:tittle>
-                    <h1 class="text-black font-bold text-lg max-w-64 break-words">{{event.name}}</h1>
+                    <h1 class="text-black font-bold text-lg max-w-64 break-words">{{ event.name }}</h1>
                 </template>
                 <template v-slot:description>
-                    <p class="text-gray-500 text-sm">{{event.description}}</p>
+                    <p class="text-gray-500 text-sm">{{ event.description }}</p>
                 </template>
             </CardUserEvent>
         </section>
@@ -59,4 +76,5 @@ load()
         <Skeleton v-if="showSkeleton" class="mt-4" width="100%" height="4rem" borderRadius="16px"></Skeleton>
         <Skeleton v-if="showSkeleton" class="mt-4" width="100%" height="4rem" borderRadius="16px"></Skeleton>
     </main>
+    <NavBarBottom></NavBarBottom>
 </template>
