426b26bf4a34dc4817da37d9f11d02cc7b9826c5
DaviSouzaES <dribeiro@olie.ai>
2024-04-17 22:14:42 -0300

    build: use toast

--- front/src/main.ts
+++ front/src/main.ts
@@ -7,12 +7,14 @@ import 'primevue/resources/themes/aura-light-green/theme.css';
 import VCalendar from 'v-calendar';
 import 'v-calendar/style.css';
 import store from './store'
+import ToastService from 'primevue/toastservice';
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.use(router);
 app.use(VCalendar, {})
 app.use(store)
+app.use(ToastService);
 
 
 app.mount('#app');
