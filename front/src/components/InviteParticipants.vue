b855770d54746b40ff1a2901fc7bf5f4a070eec8
VitoCruzOlie <vsouza@olie.ai>
2024-04-18 14:33:19 -0300

    feat- InviteParticipants component

---
+++ front/src/components/InviteParticipants.vue
@@ -0,0 +1,9 @@
+<script>
+import Button from '@/components/Button.vue';
+</script>
+<template>
+    <div class="w-full flex flex-col justify-center items-center">
+        <span class="font-semibold text-2xl">Convide participantes!</span>
+        <span class="text-neutral-500 text-sm">Convide participantes para seu evento.</span>
+    </div>
+</template>
--- front/src/views/EventDetails.vue
+++ front/src/views/EventDetails.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import DetailEventCard from '@/components/DetailEventCard.vue'
+import DetailEventCard from "@/components/DetailEventCard.vue";
+import InviteParicipants from "@/components/InviteParticipants.vue";
 </script>
 <template>
   <nav>
@@ -9,17 +10,22 @@ import DetailEventCard from '@/components/DetailEventCard.vue'
   </nav>
   <main class="w-full min-h-svh bg-white">
     <div class="gap-2.5 px-4 flex flex-col justify-center pb-4">
-      <DetailEventCard title="Reunião Top" date="18/04/2024" hour="21:20"/>
-      <div class="pl-3 flex flex-col gap-4 ">
-      <div>
-        <span class="text-sm font-semibold">ASSUNTO</span>
-        <p class="text-neutral-500 text-xs">Reunião organizacional</p>
+      <DetailEventCard title="Reunião Top" date="18/04/2024" hour="21:20" />
+      <div class="pl-3 flex flex-col gap-4">
+        <div>
+          <span class="text-sm font-semibold">ASSUNTO</span>
+          <p class="text-neutral-500 text-xs">Reunião organizacional</p>
+        </div>
+        <div>
+          <span class="text-sm font-semibold">DESCRIÇÃO DO EVENTO</span>
+          <p class="text-neutral-500 text-xs line-clamp-4">
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja.
+          </p>
+        </div>
       </div>
-      <div>
-        <span class="text-sm font-semibold">DESCRIÇÃO DO EVENTO</span>
-        <p class="text-neutral-500 text-xs line-clamp-4">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja.</p>
-      </div>
+      <InviteParicipants />
     </div>
-    </div>    
   </main>
 </template>
